34ad4b37b5716f7d5764e5c4bcb18fb9f4e1b725
Keenan Whittaker <keenman@gmail.com>
2024-09-08 15:29:36 +0100

    add Note support

---
+++ AgendaApi/Controllers/NoteController.cs
@@ -0,0 +1,66 @@
+﻿using AgendaApi;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NoteApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NoteController : ControllerBase
+    {
+        private static List<Note> Notes = [];
+
+        // GET: api/Note
+        [HttpGet]
+        public IEnumerable<Note> GetNotes()
+        {
+            return Notes;
+        }
+
+        // GET: api/Note/{id}
+        [HttpGet("{id}")]
+        public ActionResult<Note> GetNoteById(int id)
+        {
+            var Note = Notes.FirstOrDefault(a => a.Id == id);
+            if (Note == null)
+            {
+                return NotFound();
+            }
+            return Note;
+        }
+
+        // POST: api/Note
+        [HttpPost]
+        public IActionResult CreateNote([FromBody] Note newNote)
+        {
+            newNote.Id = Notes.Count + 1;
+            Notes.Add(newNote);
+            return CreatedAtAction(nameof(GetNoteById), new { id = newNote.Id }, newNote);
+        }
+
+        // PUT: api/Note/{id}
+        [HttpPut("{id}")]
+        public IActionResult UpdateNote(int id, [FromBody] Note updatedNote)
+        {
+            var Note = Notes.FirstOrDefault(a => a.Id == id);
+            if (Note == null)
+            {
+                return NotFound();
+            }
+            Note.Content = updatedNote.Content;
+            return NoContent();
+        }
+
+        // DELETE: api/Note/{id}
+        [HttpDelete("{id}")]
+        public IActionResult DeleteNote(int id)
+        {
+            var Note = Notes.FirstOrDefault(a => a.Id == id);
+            if (Note == null)
+            {
+                return NotFound();
+            }
+            Notes.Remove(Note);
+            return NoContent();
+        }
+    }
+}
--- AgendaApi/Note.cs
+++ AgendaApi/Note.cs
@@ -3,7 +3,6 @@
     public class Note
     {
         public required int Id { get; set; }
-        public string? Name { get; set; }
-        public string? Description { get; set; }
+        public string? Content { get; set; }
     }
 }
